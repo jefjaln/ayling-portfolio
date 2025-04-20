a7fcc88e8854f81fe99db7df5137ce77a8bb2cd4
Jeffrey Ayling <alaoayling@jeffs-mbp.mynetworksettings.com>
2025-04-20 17:51:27 -0400

    Initial commit

--- adp-dashboard-demo/js/server.js
+++ adp-dashboard-demo/js/server.js
@@ -7,14 +7,17 @@ app.use(cors());
 
 // Simulated API endpoint with dummy data
 app.get('/api/data', (req, res) => {
-  res.json([
-    { label: 'January', value: 50 },
-    { label: 'February', value: 75 },
-    { label: 'March', value: 150 },
-    { label: 'April', value: 100 },
-  ]);
+  res.json({
+    labels: ['Month 1', 'Month 2', 'Month 3', 'Month 4', 'Month 5', 'Month 6'],
+    values1: [0, 60, 85, 90, 100, 100],
+    values2: [0, 50, 70, 80, 90, 100],
+    barLabels: ['Toll Brother Luxury Homes Inc | 2009-2014', 'J.W. Pepper | 2014-2020', 'Synopsys Inc | 2021-2025'],
+    barValues: [5, 6, 4],
+    circleLabels: ['UX/UX Design', 'HTML', 'CSS', 'JavaScript'],
+    circleValues: [5, 5, 5, 5],
+  });
 });
 
 app.listen(3001, () => {
   console.log('Server is running on http://localhost:3001');
-});
+});1234
